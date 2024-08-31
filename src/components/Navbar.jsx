598e1b6ce7eef6c28296aadf9f9554b3e2a543d1
Krunalpits <125281938+Krunalpits@users.noreply.github.com>
2024-08-31 15:48:54 -0700

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,11 @@
-import logo from "../assets/KevinRushLogo.png";
+// import logo from "../assets/KevinRushLogo.png";
 import { FaLinkedin} from "react-icons/fa";
 
 const Navbar = () => {
     return (
         <nav className="mb-20 flex items-center justify-between py-6">
             <div className="flex flex-shrink-0 items-center">
-                <img src={logo} alt="Logo" className="my-2 mx-10 w-10" />
+                <img alt="Logo" className="my-2 mx-10 w-10" />
             </div>
             <div className="flex items-center gap-4 text-2xl ml-auto mr-6">
                 <a href="https://www.linkedin.com/in/krunal-pithadia-284926250/" target="_blank" rel="noopener noreferrer">
